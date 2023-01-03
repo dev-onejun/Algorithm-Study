2321094a6d1de1360ce9e5816ab115b4eb748dd6
dev-onejun <wjjgjk@naver.com>
2023-01-03 18:00:57 +0900

    SOLVE: 모험가 길드

---
+++ tutorials/모험가 길드.py
@@ -0,0 +1,17 @@
+# 공포도 내림차순 sort, 그룹 만들기
+
+N = int(input())
+fears = list(map(int, input().split()))
+
+fears.sort()
+
+answer = 0
+group = []
+for fear in fears:
+    group.append(fear)
+
+    if len(group) >= fear:
+        answer += 1
+        group.clear()
+
+print(answer)
