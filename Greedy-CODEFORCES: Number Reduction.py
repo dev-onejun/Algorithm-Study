b963700f7b62ca6526473ada3ce75ab1acc90806
dev-onejun <wjjgjk@naver.com>
2023-01-04 19:15:34 +0900

    TRY 2: Number Reduction

--- Greedy-CODEFORCES: Number Reduction.py
+++ Greedy-CODEFORCES: Number Reduction.py
@@ -1,40 +1,40 @@
 """
 " https://codeforces.com/problemset/problem/1765/N
+
 " Delete from 9 to 0
     - making exceptions according to the problem conditions.
     - FAILED.
+
+" Delete the maximum number in X[:k+1]
+    - the key idea to make the minimum positive integer is just descending the number of digits.
+    - and the possible digit to remove is only in the range of 0 ~ k.
+
+    - continuously WA on test 2
 """
 t = int(input())
 
+answer = []
+
 while t:
-    x = list(input())
+    x = list(input().lstrip('0'))
     k = int(input())
 
-    for num in range(9, -1, -1):
-        if k == 0:
+    n = len(x)
+    for i in range(k):
+        if n <= 1:
             break
 
-        while True:
-            try:
-                target = x.index(str(num))
-            except:
-                target = -1
+        target = max(x[:n - k])
 
-            if target == 0 and x[1] == '0':
-                try:
-                    target = x.index(str(num), 2)
-                except:
-                    target = -1
+        if x.index(target) == 0 and x[1] == '0':
+            target = max(x[1: n-k+1])
 
-            if target == -1 or k == 0:
-                break
+        x.remove(target)
 
-            x.pop(target)
-            k -= 1
-
-    result = ''
-    for char in x:
-        result += char
-    print(result)
+    result = ''.join(x)
+    answer.append(result)
 
     t -= 1
+
+for output in answer:
+    print(output)
