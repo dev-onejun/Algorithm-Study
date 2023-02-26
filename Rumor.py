83c67f61c0252c6ccaec201634c336be69e110e5
dev-onejun <wjjgjk@naver.com>
2023-02-27 05:20:53 +0900

    Try 2: Rumor (#12)

--- Rumor.py
+++ Rumor.py
@@ -1,24 +1,42 @@
+from collections import deque
 from typing import List, Tuple
-import sys
-sys.setrecursionlimit(10**7)
 
-def dfs(v: Tuple, visited: List[bool]) -> int:
+def dfs(graph: List[List[int]], v: Tuple, visited: List[bool]) -> int:
     idx: int = v[0]
     bribe: int = v[1]
 
     visited[idx] = True
+
     for i in graph[idx]:
         if not visited[i]:
-            visited[i] = True
-            dfs((i, c[i-1]), visited)
+            #dfs(graph, (i, c[i-1]), visited)
+            return min(bribe, dfs(graph, (i, c[i]), visited) )
 
     return bribe
+
+def bfs(graph, start: Tuple[int, int], visited):
+    queue = deque([start])
+
+    min_bribe = 1000000001
+    while queue:
+        v = queue.popleft()
+
+        idx = v[0]
+        bribe = v[1]
+
+        visited[idx] = True
+        min_bribe = min(min_bribe, bribe)
+
+        for character in graph[idx]:
+            if not visited[character]:
+                queue.append( (character, c[character]) )
+
+    return min_bribe
 
 # n : the number of chracters in Overcity
 # m : the number of pairs of friends
 n, m = map(int, input().split())
 c = list(map(int, input().split() ) )
-print(c)
 
 #graph: List[List[int]] = [ [] ] * (n+1)
 graph: List[List[int]] = [ [] for i in range(n+1) ]
@@ -26,19 +44,14 @@ for i in range(m):
     n1, n2 = map(int, input().split() )
     graph[n1].append(n2)
     graph[n2].append(n1)
-print(graph)
 
-characters: List[Tuple] = [] # (index, bribe)
-for idx, bribe in enumerate(c):
-    characters.append( (idx + 1, bribe) )
-characters.sort(key=lambda character: character[1]) # sort by bribe
-print(characters)
-
-visited = [False] * (len(c) + 1)
-visited[0] = True
+c.insert(0, 0)
+visited = [False] * len(c)
 answer: int = 0
-for character in characters:
-    if not visited[character[0]]:
-        answer += dfs(character, visited)
+visited[0] = True
+for idx, bribe in enumerate(c):
+    if not visited[idx]:
+        #answer += dfs(graph, (idx, bribe), visited)
+        answer += bfs(graph, (idx, bribe), visited)
 
 print(answer)
